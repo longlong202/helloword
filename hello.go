e233415c1fb93e6ba80442de417d9956fa7efa86
longlong202 <ustblonglong@sina.com>
2019-04-11 22:36:22 +0800

    rune test

--- hello.go
+++ hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Helllo,world!")
+	var s = "我爱China!"
+	for i,ch := range []rune(s){
+		fmt.Printf("(%d %c)",i,ch)
+	}
+	fmt.Println()
+	x :=[]int{
+		1, 2, 3,
+		4, 5, 6,
+	}
+	for _,v:= range x{
+		fmt.Println(v)
+	}
 }
